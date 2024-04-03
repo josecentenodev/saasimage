56ab10888b0ac0d5d8a2d2bac8f806ff9208dcb6
Centeno Jose Antonio <jocecito182@gmail.com>
2024-04-03 15:36:02 -0300

    Add server actions and mongodb conn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,9 @@
 import React from "react";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 const Home = () => {
   return (
     <div>
       <p>Home</p>
-      <SignedIn>
-        {/* Mount the UserButton component */}
-        <UserButton />
-      </SignedIn>
-      <SignedOut>
-        {/* Signed out users get sign in button */}
-        <SignInButton />
-      </SignedOut>
     </div>
   );
 };
